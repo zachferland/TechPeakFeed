f9984efb35d523e90387aac4b295bbd0a7c956c3
Zach Ferland <zachferland@gmail.com>
2013-07-18 17:58:16 +0200

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,13 @@
 
 
 User.create(name: 'Bianca Welds', image: 'bianca-welds.png', feed: 'http://blog.biancawelds.com/feed/')
+
+User.create(name: 'Angel Milev', image: 'angel.png', feed: 'http://secondscreeninsider.com/feed/')
+
+User.create(name: 'Davide Rizzo', image: 'davide-rizzo.png', feed: 'http://daviderizzo.com/feed/')
+
+User.create(name: 'Giancarlo Frison', image: 'giancarlo-frison.png', feed: 'http://gfrison.com/feed/')
+
+User.create(name: 'Nejc Škoberne', image: 'nejc-skoberne.png', feed: 'http://nejc.skoberne.net/feed/')
+
+User.create(name: 'Pavel Šimeček', image: 'pavel-simecek.png', feed: 'http://blog.pavelsimecek.cz/?feed=rss2')
