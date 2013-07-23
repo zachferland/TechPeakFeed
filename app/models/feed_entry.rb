9a0fbd89b9ad4f83e744e4d1324c81fb6b25d5f5
Zach Ferland <zachferland@gmail.com>
2013-07-23 14:44:51 +0200

    Clean up

--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -9,7 +9,7 @@ class FeedEntry < ActiveRecord::Base
   		feed_url = user.feed
   		feed = Feedzirra::Feed.fetch_and_parse(feed_url)
   		add_entries(feed.entries, user.id)
-	end
+	  end
   end
 
   def self.update_feed
@@ -24,27 +24,36 @@ class FeedEntry < ActiveRecord::Base
   def self.add_entries(entries, user)
   	entries.each do |entry|
       unless exists? :guid => entry.id	
-        create!(
-          :name         => entry.title,
-          :summary      => clean_summary(entry),
-          :url          => entry.url,
-          :published_at => entry.published,
-          :guid         => entry.id,
-          :publisher_id  => user
-        )
+        create_entry(entry, user)
       end
     end
   end
 
   private 
-  def self.clean_summary(entry)
-  	#atom must access content, while rss should access summary
-    post_content = entry.summary
-    post_content = entry.content if post_content.nil?
+  def self.create_entry(entry, user)
+    create!(
+      :name          => entry.title,
+      :summary       => clean_summary(entry),
+      :url           => entry.url,
+      :published_at  => entry.published,
+      :guid          => entry.id,
+      :publisher_id  => user
+    )
+  end
 
-    stripped_summary = strip_tags(post_content)
-    decoder = HTMLEntities.new
-    decoder.decode(stripped_summary)
+  private 
+  def self.clean_summary(entry)
+    summary = get_summary(entry)
+    stripped_summary = strip_tags(summary)
+    HTMLEntities.new.decode(stripped_summary)
+  end
+
+  private 
+  def self.get_summary(entry)
+    #atom must access content, while rss should access summary
+    post_content = entry.summary
+    post_content = entry.content if entry.summary.nil?
+    post_content
   end
 
 end
